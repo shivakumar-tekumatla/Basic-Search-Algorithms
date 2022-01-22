d8fc4835cf5ec37ba48a414665e7e2636321df0b
Reanu-Keeves <55010991+Reanu-Keeves@users.noreply.github.com>
2022-01-22 10:34:56 -0500

    Initial Commit

---
+++ search.py
@@ -0,0 +1,148 @@
+# Basic searching algorithms
+
+def bfs(grid, start, goal):
+    '''Return a path found by BFS alogirhm 
+       and the number of steps it takes to find it.
+
+    arguments:
+    grid - A nested list with datatype int. 0 represents free space while 1 is obstacle.
+           e.g. a 3x3 2D map: [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+    start - The start node in the map. e.g. [0, 0]
+    goal -  The goal node in the map. e.g. [2, 2]
+
+    return:
+    path -  A nested list that represents coordinates of each step (including start and goal node), 
+            with data type int. e.g. [[0, 0], [0, 1], [0, 2], [1, 2], [2, 2]]
+    steps - Number of steps it takes to find the final solution, 
+            i.e. the number of nodes visited before finding a path (including start and goal node)
+
+    >>> from main import load_map
+    >>> grid, start, goal = load_map('test_map.csv')
+    >>> bfs_path, bfs_steps = bfs(grid, start, goal)
+    It takes 10 steps to find a path using BFS
+    >>> bfs_path
+    [[0, 0], [1, 0], [2, 0], [3, 0], [3, 1]]
+    '''
+    ### YOUR CODE HERE ###
+    path = []
+    steps = 0
+    found = False
+
+    if found:
+        print(f"It takes {steps} steps to find a path using BFS")
+    else:
+        print("No path found")
+    return path, steps
+
+
+def dfs(grid, start, goal):
+    '''Return a path found by DFS alogirhm 
+       and the number of steps it takes to find it.
+
+    arguments:
+    grid - A nested list with datatype int. 0 represents free space while 1 is obstacle.
+           e.g. a 3x3 2D map: [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+    start - The start node in the map. e.g. [0, 0]
+    goal -  The goal node in the map. e.g. [2, 2]
+
+    return:
+    path -  A nested list that represents coordinates of each step (including start and goal node), 
+            with data type int. e.g. [[0, 0], [0, 1], [0, 2], [1, 2], [2, 2]]
+    steps - Number of steps it takes to find the final solution, 
+            i.e. the number of nodes visited before finding a path (including start and goal node)
+
+    >>> from main import load_map
+    >>> grid, start, goal = load_map('test_map.csv')
+    >>> dfs_path, dfs_steps = dfs(grid, start, goal)
+    It takes 9 steps to find a path using DFS
+    >>> dfs_path
+    [[0, 0], [0, 1], [0, 2], [1, 2], [2, 2], [2, 3], [3, 3], [3, 2], [3, 1]]
+    '''
+    ### YOUR CODE HERE ###
+    path = []
+    steps = 0
+    found = False
+
+    if found:
+        print(f"It takes {steps} steps to find a path using DFS")
+    else:
+        print("No path found")
+    return path, steps
+
+
+def dijkstra(grid, start, goal):
+    '''Return a path found by Dijkstra alogirhm 
+       and the number of steps it takes to find it.
+
+    arguments:
+    grid - A nested list with datatype int. 0 represents free space while 1 is obstacle.
+           e.g. a 3x3 2D map: [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+    start - The start node in the map. e.g. [0, 0]
+    goal -  The goal node in the map. e.g. [2, 2]
+
+    return:
+    path -  A nested list that represents coordinates of each step (including start and goal node), 
+            with data type int. e.g. [[0, 0], [0, 1], [0, 2], [1, 2], [2, 2]]
+    steps - Number of steps it takes to find the final solution, 
+            i.e. the number of nodes visited before finding a path (including start and goal node)
+
+    >>> from main import load_map
+    >>> grid, start, goal = load_map('test_map.csv')
+    >>> dij_path, dij_steps = dijkstra(grid, start, goal)
+    It takes 10 steps to find a path using Dijkstra
+    >>> dij_path
+    [[0, 0], [1, 0], [2, 0], [3, 0], [3, 1]]
+    '''
+    ### YOUR CODE HERE ###
+    path = []
+    steps = 0
+    found = False
+
+    if found:
+        print(f"It takes {steps} steps to find a path using Dijkstra")
+    else:
+        print("No path found")
+    return path, steps
+
+
+def astar(grid, start, goal):
+    '''Return a path found by A* alogirhm 
+       and the number of steps it takes to find it.
+
+    arguments:
+    grid - A nested list with datatype int. 0 represents free space while 1 is obstacle.
+           e.g. a 3x3 2D map: [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+    start - The start node in the map. e.g. [0, 0]
+    goal -  The goal node in the map. e.g. [2, 2]
+
+    return:
+    path -  A nested list that represents coordinates of each step (including start and goal node), 
+            with data type int. e.g. [[0, 0], [0, 1], [0, 2], [1, 2], [2, 2]]
+    steps - Number of steps it takes to find the final solution, 
+            i.e. the number of nodes visited before finding a path (including start and goal node)
+
+    >>> from main import load_map
+    >>> grid, start, goal = load_map('test_map.csv')
+    >>> astar_path, astar_steps = astar(grid, start, goal)
+    It takes 7 steps to find a path using A*
+    >>> astar_path
+    [[0, 0], [1, 0], [2, 0], [3, 0], [3, 1]]
+    '''
+    ### YOUR CODE HERE ###
+    path = []
+    steps = 0
+    found = False
+
+    if found:
+        print(f"It takes {steps} steps to find a path using A*")
+    else:
+        print("No path found")
+    return path, steps
+
+
+# Doctest
+if __name__ == "__main__":
+    # load doc test
+    from doctest import testmod, run_docstring_examples
+    # Test all the functions
+    testmod()
