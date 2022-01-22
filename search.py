9299a81ea230969cf062545617d6a7e22a5ef3f4
Reanu-Keeves <55010991+Reanu-Keeves@users.noreply.github.com>
2022-01-22 14:14:19 -0500

    Add BFS function
    Added the program in BFS function.

--- search.py
+++ search.py
@@ -1,4 +1,8 @@
 # Basic searching algorithms
+from operator import truediv
+from xml.dom import NotFoundErr
+
+
 def graph(grid):
     """
     Returns the graph for a given grid. 
@@ -62,10 +66,76 @@ def bfs(grid, start, goal):
     [[0, 0], [1, 0], [2, 0], [3, 0], [3, 1]]
     '''
     ### YOUR CODE HERE ###
+    graph_nodes = graph(grid)
     
+    queue =[]
+    visited=[]
     path = []
+    path_track={}
     steps = 0
     found = False
+
+    queue.append(start)
+    visited.append(start)
+
+    while queue or not found:
+        item = queue.pop(0)    # Dequeuing the first element in the queue
+        if item == goal:
+            found = True
+            break 
+        for node in graph_nodes[tuple(item)]:
+            if node not in visited:
+                visited.append(node)
+                queue.append(node)
+                path_track[tuple(node)] = item  # Keeping track of the parents of each node 
+                
+                if node == goal:
+                    found = True
+                    break
+                else:
+                    steps+=1
+
+    # BFS exploration is done. Shortest path in BFS should be computed 
+    path.append(goal)
+    node=goal
+    if found:
+        while node!=start:
+            node = path_track[tuple(node)]
+            path.append(node)
+    else:
+        path=[]
+    
+    path.reverse()
+
+
+
+    # while not found or  queue:
+    #     item = queue.pop(0)
+    #     # if item == goal:
+    #     #     found = True
+    #     #     steps+=1
+    #     #     break
+    #     for node in graph_nodes[tuple(item)]:
+    #         if node not in visited:
+    #             visited.append(node)
+    #             queue.append(node)
+    #             steps+=1
+    #             if node == goal:
+    #                 found = True
+    #                 break
+
+
+    # path=visited         
+
+    # for item in queue:
+    #     if item not in path and not found:
+    #         path.append(item)
+    #         steps+=1
+    #         for val in graph_nodes[tuple(item)]:
+    #             queue.append(val)
+    #         if item ==goal:
+    #             found = True
+    #             break
 
     if found:
         print(f"It takes {steps} steps to find a path using BFS")
